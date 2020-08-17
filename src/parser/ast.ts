098ec2651f10524100d2ae9779f0f28849235462
VimHax <45242122+VimHax@users.noreply.github.com>
2020-08-17 22:10:04 +0530

    Added a bunch of nodes

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -1,5 +1,25 @@
 import { Token, Span } from '../lexer/token';
 
+// DataType //
+
+export type DataType = 'string' | 'number' | 'boolean' | 'instruction' | FunctionDataType | ClosureDataType | ArrayDataType;
+
+export interface FunctionDataType {
+	parameters: DataType[];
+	returnType: DataType;
+}
+
+export interface ClosureDataType {
+	parameters: DataType[];
+	returnType: DataType;
+}
+
+export interface ArrayDataType {
+	datatype: DataType;
+}
+
+// RValue Node //
+
 export interface RValueVariableNode {
 	name: Token;
 }
@@ -13,18 +33,43 @@ export interface RValueIndexOfNode {
 export interface RValueCallNode {
 	rvalue: RValueNode;
 	arguments: ExpressionNode[];
+	span: Span;
 }
 
 export type RValueNode = RValueVariableNode | RValueIndexOfNode;
 
+// Literal Node //
+
 export interface StringLiteralNode {
 	value: string;
 	datatype: 'string';
+	span: Span;
 }
 
 export interface NumberLiteralNode {
 	value: number;
 	datatype: 'number';
+	span: Span;
+}
+
+export interface BooleanLiteralNode {
+	value: boolean;
+	datatype: 'boolean';
+	span: Span;
+}
+
+export interface FunctionLiteralNode {
+	parameters: [string, DataType][];
+	returnType: DataType;
+	instructions: InstructionNode;
+	span: Span;
+}
+
+export interface ClosureLiteralNode {
+	parameters: [string, DataType][];
+	returnType: DataType;
+	instructions: InstructionNode;
+	span: Span;
 }
 
 export interface ArrayLiteralNode {
@@ -32,11 +77,14 @@ export interface ArrayLiteralNode {
 	span: Span;
 }
 
+// Expression Node //
+
 type UnaryOperator = 'plus' | 'minus';
 
 export interface UnaryNode {
 	operator: UnaryOperator;
 	operand: ExpressionNode;
+	span: Span;
 }
 
 type BinaryOperator = 'add' | 'subtract' | 'multiply' | 'divide' | 'modulus' | 'and' | 'or' | 'equal' | 'notequal' | 'lessthan' | 'greaterthan' | 'lessthanorequal' | 'greaterthanorequal';
@@ -44,9 +92,12 @@ type BinaryOperator = 'add' | 'subtract' | 'multiply' | 'divide' | 'modulus' | '
 export interface BinaryNode {
 	operator: BinaryOperator;
 	operands: [ExpressionNode, ExpressionNode];
+	span: Span;
 }
 
-type ExpressionNode = RValueNode | StringLiteralNode | NumberLiteralNode | ArrayLiteralNode | UnaryNode | BinaryNode;
+type ExpressionNode = RValueNode | StringLiteralNode | NumberLiteralNode | ArrayLiteralNode | UnaryNode | BinaryNode | InstructionNode;
+
+// LValue Node //
 
 export interface LValueVariableNode {
 	name: Token;
@@ -59,3 +110,46 @@ export interface LValueIndexOfNode {
 }
 
 export type LValueNode = LValueVariableNode | LValueIndexOfNode;
+
+// Instruction Node //
+
+export interface PrintInstructionNode {
+	expressions: ExpressionNode[];
+	span: Span;
+}
+
+export interface AssignInstructionNode {
+	lvalue: LValueNode;
+	expression: ExpressionNode;
+	span: Span;
+}
+
+export interface EvaluateInstructionNode {
+	expression: ExpressionNode;
+	span: Span;
+}
+
+export interface ReturnInstructionNode {
+	expression: ExpressionNode;
+	span: Span;
+}
+
+export interface ExecuteInstructionNode {
+	instructions: InstructionNode;
+	span: Span;
+}
+
+export interface IfInstructionNode {
+	condition: ExpressionNode;
+	then: InstructionNode;
+	else: InstructionNode | null;
+	span: Span;
+}
+
+export interface WhileInstructionNode {
+	condition: ExpressionNode;
+	body: InstructionNode;
+	span: Span;
+}
+
+export type InstructionNode = PrintInstructionNode | AssignInstructionNode | EvaluateInstructionNode | ReturnInstructionNode | ExecuteInstructionNode | ArrayLiteralNode;
